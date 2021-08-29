b16a49487a52f187f72bc1a77b0c7b8bdafc2685
koliadjun <koliadjun@gmail.com>
2021-08-29 22:34:53 +0300

    button done

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+const Button = ({ children, onClick }) => {
+  return (
+    <button type="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+Button.prpsType = {
+  children: PropTypes.element,
+  onClick: PropTypes.func.isRequired,
+};
+export default Button;
