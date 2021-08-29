ed794272f8f6710984d413f3501bea91d97b6cc6
koliadjun <koliadjun@gmail.com>
2021-08-29 22:25:03 +0300

    fix children props

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const ImageGallery = () => {
-  return <ul className="ImageGallery">{this.props.children}</ul>;
+const ImageGallery = ({ children }) => {
+  return <ul className="ImageGallery">{children}</ul>;
 };
 
 ImageGallery.propTypes = { children: PropTypes.element };
