e8b0fe3b9a32347627136d54831426ba79ca0e8e
Koliadjun <koliadjun@gmail.com>
2021-09-09 23:16:13 +0300

    fixed props type adn destructurin

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 export class ImageGalleryItem extends Component {
-  //   static propTypes = { image: PropTypes.array.isRequired };
+  static propTypes = {
+    key: PropTypes.string.isRequired,
+    src: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+    largeImg: PropTypes.string.isRequired,
+  };
 
   render() {
-    const { src, onClick } = this.props;
+    const { src, onClick, largeImg } = this.props;
     return (
-      <li
-        className="ImageGalleryItem"
-        onClick={() => onClick(this.props.largeImg)}
-      >
+      <li className="ImageGalleryItem" onClick={() => onClick(largeImg)}>
         <img src={src} alt="" className="ImageGalleryItem-image" />
       </li>
     );
